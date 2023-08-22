8ab480aa776b369e098fb59164156eec51a67daf
Selecia aaa <garrisonroniece@gmail.com>
2023-08-22 11:33:22 -0400

    js completed and fully functioning

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,18 @@
 // refernce html
+// create variables
+// create functoion to start quiz
+// create a function to load answer choices
+// fuynction to check answer
+// function to decrease time 
+// save with local storage
+// !!!!Make sure to add ul in html to display saved scores!!!
+
 var questionText = document.getElementById("question-text");
 var optionsList = document.getElementById("options-list");
 var startButton = document.getElementById("start-button");
-
-// create variables
+var timerElement = document.getElementById("timer");
+var timeRemainingElement = document.getElementById("time-remaining");
+var highScoresList = document.getElementById("high-scores-list");
 
 var quizQuestions = [
     {
@@ -33,8 +42,105 @@ var quizQuestions = [
     },
 ];
 
-// create functoion to start quiz
-// create a function to load answer choices
-// fuynction to check answer
-// function to decrease time 
-// save with local storage
+var currentQuestionIndex = 0;
+var score = 0;
+var timer;
+var timeLimit = 60;
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  
+  timer = setInterval(updateTimer, 1000);
+
+  displayQuestion(currentQuestionIndex);
+}
+
+function displayQuestion(index) {
+  var question = quizQuestions[index];
+  questionText.textContent = question.question;
+
+  optionsList.innerHTML = "";
+  question.options.forEach(function(option, optionIndex) {
+    var optionItem = document.createElement("li");
+    optionItem.textContent = option;
+    optionItem.addEventListener("click", function() {
+      checkAnswer(option, question.answer);
+    });
+    optionsList.appendChild(optionItem);
+  });
+}
+
+function checkAnswer(selectedOption, correctAnswer) {
+  if (selectedOption === correctAnswer) {
+    score++;
+  } else {
+    // Subtract time for incorrect answer
+    timeLimit -= 10;
+  }
+
+  
+  currentQuestionIndex++;
+  if (currentQuestionIndex < quizQuestions.length) {
+    displayQuestion(currentQuestionIndex);
+  } else {
+    endQuiz();
+  }
+}
+
+function updateTimer() {
+  timerElement.textContent = "Time: " + timeLimit + "s";
+
+  if (timeLimit <= 0) {
+    endQuiz();
+  } else {
+    timeLimit--;
+  }
+}
+
+function endQuiz() {
+  clearInterval(timer);
+
+  var quizContainer = document.getElementById("quiz-container");
+  quizContainer.innerHTML = "<h2>Quiz Over</h2>" +
+    "<p>Your score: " + score + "</p>" +
+    "<label for='initials'>Enter your initials:</label>" +
+    "<input type='text' id='initials'>" +
+    "<button id='save-button'>Save Score</button>";
+
+  var saveButton = document.getElementById("save-button");
+  saveButton.addEventListener("click", saveScore);
+}
+
+function saveScore() {
+  var initialsInput = document.getElementById("initials");
+  var initials = initialsInput.value;
+
+  if (initials !== "") {
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    var newScore = {
+      initials: initials,
+      score: score
+    };
+
+    highScores.push(newScore);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+   
+    initialsInput.value = "";
+    displayHighScores();
+  }
+}
+
+function displayHighScores() {
+  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+  highScoresList.innerHTML = "";
+  highScores.forEach(function(score) {
+    var listItem = document.createElement("li");
+    listItem.textContent = score.initials + ": " + score.score;
+    highScoresList.appendChild(listItem);
+  });
+}
